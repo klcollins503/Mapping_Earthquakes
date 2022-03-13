7c23a8d9eb7e0471c377a0dae5c4de862da656ed
Kim Collins <klcollins502@gmail.com>
2022-03-13 16:38:25 -0700

    adding files to mapping single points branch

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,27 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffeda0',
+    fillOpacity: 0.5,
+    radius: 300
+ }).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+//add the layer to map
+streets.addTo(map);
